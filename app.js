82e987cd837468cf5c993038edc1047bf79215e1
Keshav Bajaj <bajajkeshav111@gmail.com>
2024-09-25 23:32:53 +0530

    basic website initialised

---
+++ app.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#89cc04"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+ctx.fillStyle = "black"
+ctx.filter = "blur(1.5px)";
+let check = document.getElementById("check")
+ctx.font = "90px Arial Narrow";
+
+setInterval(()=>{
+  ctx.clearRect(0,0,500,500)
+  ctx.fillStyle = "#89cc04"
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = "black"
+  let text = document.querySelector("#text").value || "brat"
+  check.innerHTML = text
+  let width = check.getBoundingClientRect().width
+  ctx.save();
+  ctx.scale(100 / 135, 1)
+  ctx.textAlign = "center"
+  ctx.fillText(text, canvas.width / 2 * (135 / 100), canvas.width / 2 + 15)
+  ctx.restore();
+},200)
+
+
+function downloadBrat(){
+    var canvas1 = document.getElementById("board")
+    
+    var imageObject = new Image()
+    imageObject.src = canvas1.toDataURL("image/png")
+
+    var imageElement = document.createElement("img")
+    imageElement.src = imageObject.src
+
+    let link = document.createElement("a")
+    link.setAttribute('download', "download")
+    link.href= imageElement.src
+    link.click()
+}
+document.querySelector("#download").addEventListener("click",downloadBrat)
